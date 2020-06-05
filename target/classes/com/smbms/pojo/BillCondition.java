1088b8156de7d23a33daccee4b49abe0a977e33f
wu <2212917677@qq.com>
2020-06-05 19:21:23 +0800

    spring-mvc整合项目第三次提交

---
+++ target/classes/com/smbms/pojo/BillCondition.java
@@ -0,0 +1,82 @@
+package com.smbms.pojo;
+
+/**
+ * @program: smbms-maven-demo
+ * @description:
+ * @author:WF
+ * @create: 2020-06-05 18:06
+ **/
+public class BillCondition {
+    private Integer id;
+    private String billCode;
+    private String productName;
+    private String productUnit;
+    private Integer productCount;
+    private Integer totalPrice;
+    private Integer providerId;
+    private Integer isPayment;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBillCode() {
+        return billCode;
+    }
+
+    public void setBillCode(String billCode) {
+        this.billCode = billCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductUnit() {
+        return productUnit;
+    }
+
+    public void setProductUnit(String productUnit) {
+        this.productUnit = productUnit;
+    }
+
+    public Integer getProductCount() {
+        return productCount;
+    }
+
+    public void setProductCount(Integer productCount) {
+        this.productCount = productCount;
+    }
+
+    public Integer getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Integer totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(Integer providerId) {
+        this.providerId = providerId;
+    }
+
+    public Integer getIsPayment() {
+        return isPayment;
+    }
+
+    public void setIsPayment(Integer isPayment) {
+        this.isPayment = isPayment;
+    }
+}
